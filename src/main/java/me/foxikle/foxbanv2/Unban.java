ece5a032ad622fe06d3b8ceafbecbefeb2a26aea
BuildTools <unconfigured@null.spigotmc.org>
2022-08-25 14:41:24 -0400

    Added unban logging.

--- src/main/java/me/foxikle/foxbanv2/Unban.java
+++ src/main/java/me/foxikle/foxbanv2/Unban.java
@@ -8,35 +8,44 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
 
 import static me.foxikle.foxrank.FoxRank.getRank;
 
 public class Unban implements CommandExecutor, TabExecutor {
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (label.equalsIgnoreCase("fban")) {
+        if (label.equalsIgnoreCase("unban")) {
             if (sender instanceof Player) {
-                Player unBaner = (Player) sender;
-                if (getRank(unBaner).getPowerLevel() >= 90) {
+                Player staff = (Player) sender;
+                if (getRank(staff).getPowerLevel() >= 90) {
                     if (args.length >=1) {
                         Bukkit.getServer().getOfflinePlayer(args[0]);
-                        OfflinePlayer unbanee = Bukkit.getServer().getOfflinePlayer(args[0]);
-                        if (unbanee.isBanned()) {
-                            Bukkit.getBanList(BanList.Type.NAME).pardon(unbanee.getName());
-                            Bukkit.getBanList(BanList.Type.IP).pardon(unbanee.getName());
+                        OfflinePlayer player = Bukkit.getServer().getOfflinePlayer(args[0]);
+                        if (player.isBanned()) {
+                            Bukkit.getBanList(BanList.Type.NAME).pardon(player.getName());
+                            Bukkit.getBanList(BanList.Type.IP).pardon(player.getName());
+                            staff.sendMessage(ChatColor.RED + player.getName() + " was unbanned.");
+                            addAuditLogEntry(staff, player);
                         } else{
-                            unBaner.sendMessage(ChatColor.RED + "This player is not banned.");
+                            staff.sendMessage(ChatColor.RED + "This player is not banned.");
                         }
                     } else {
-                        unBaner.sendMessage(ChatColor.RED + "Incorrect usage: /unban <PLAYER>");
+                        staff.sendMessage(ChatColor.RED + "Incorrect usage: /unban <PLAYER>");
                     }
                 } else {
-                    unBaner.sendMessage(ChatColor.RED + "You must have ADMIN rank for higher to use this command.");
-                    unBaner.sendMessage(ChatColor.RED + "Please contact a server administrator is you think this is a mistake.");
+                    staff.sendMessage(ChatColor.RED + "You must have ADMIN rank for higher to use this command.");
+                    staff.sendMessage(ChatColor.RED + "Please contact a server administrator is you think this is a mistake.");
                 }
                 return true;
             }
@@ -47,14 +56,34 @@ public class Unban implements CommandExecutor, TabExecutor {
         return true;
     }
 
+    private void addAuditLogEntry(Player s, OfflinePlayer p){
+
+
+            File file = new File("plugins/FoxBanV2/AuditLog.yml");
+            YamlConfiguration ymlConfig = YamlConfiguration.loadConfiguration(file);
+            Map<String, String> map = new HashMap<>();
+            map.put("Staff", s.getName());
+            map.put("Player", p.getName());
+            map.put("Time", Instant.now().toString());
+
+            ymlConfig.createSection("UNBAN: " + p.getUniqueId(), map);
+
+            try {
+                ymlConfig.save(file);
+            } catch (IOException e) {
+                Bukkit.getLogger().log(Level.SEVERE, "Failed to save audit log entry: UNBAN ", e);
+            }
+        }
+
+
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         if (args.length == 1){
             List<String> playerNames = new ArrayList<>();
-            OfflinePlayer[] players = new Player[Bukkit.getServer().getBannedPlayers().size()];
+            OfflinePlayer[] players = new OfflinePlayer[Bukkit.getServer().getBannedPlayers().size()];
             Bukkit.getServer().getBannedPlayers().toArray(players);
-            for (int i = 0; i < players.length; i++){
-                playerNames.add(players[i].getName());
+            for (OfflinePlayer player : players) {
+                playerNames.add(player.getName());
             }
             return playerNames;
         }
